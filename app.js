f21b9f3f31d04a6ac944b6a5510195577c05697f
Rahulparmar01 <rahul220.iit@gmail.com>
2020-04-09 22:10:17 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,43 @@
+//alert("hello peeps");
+ 
+
+
+$(document).ready(function () {
+
+    
+    let totalExpense=00;
+    const eList=$("#expense")
+    
+
+
+$("#AddBtn").click(function () { 
+    const  reasonval=$("#reason").val();
+    const amount =$("#amount").val();
+
+    if(reasonval==""||amount<=0){
+        return;
+    }
+
+    var ele = document.createElement("ion-item")
+    ele.textContent=reasonval + ": $"+amount;
+    eList.append(ele);
+    console.log(totalExpense);
+    
+    totalExpense+= +amount;
+    console.log(totalExpense);
+    $("#totexp span").text(totalExpense);
+    
+
+    
+    $("#reason").val('');
+    $("#amount").val('');
+});
+
+$("#clearBtn").click(function () { 
+    console.log("works");
+    
+    eList.empty();
+});
+    
+});
+
